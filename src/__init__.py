ecb0157d8a420233e5de1af267dad6b0d83b9a58
Santiago Giraldo Henao <155782607+SantiagoGiraldoH@users.noreply.github.com>
2025-11-11 23:08:30 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,15 @@
+"""
+Adaptive Attention Span in Transformers
+Implementation for learning purposes
+"""
 
+__version__ = "0.1.0"
+__author__ = "Santiago Giraldo Henao"
+
+from .adaptive_span import AdaptiveSpan
+from .models import AdaptiveTransformer
+
+__all__ = [
+    "AdaptiveSpan",
+    "AdaptiveTransformer",
+]
